0fab921d1919c13c5c2ec24b488e71bb097098d6
BiPolar_Bear <izabela.bratovic@gmail.com>
2014-07-27 21:32:24 +0200

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+    $(".day").css("height", $(window).height());
+
+    $(".movie").mouseenter(function() {
+        $( this ).children('.title').css('opacity', 0.9);
+    })
+    $(".movie").mouseleave(function () {
+        $(this).children('.title').css('opacity', 0);
+  });
+
+});
+
+$(window).resize(function () {
+
+    $(".day").css("height", $(window).height());
+
+});
