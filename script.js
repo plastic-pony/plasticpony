35ef55d1658f7ac07aafff2d618392c8bd80d65a
plasticpony <izabela.bratovic@gmail.com>
2014-08-17 11:19:55 +0200

    prema programu

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
     $(".day").css("height", $(window).height());
 
     $(".movie").mouseenter(function() {
-        $( this ).children('.title').css('opacity', 0.9);
+        $(this).children('.title').css('opacity', 0.9);
     })
     $(".movie").mouseleave(function () {
         $(this).children('.title').css('opacity', 0);
@@ -16,3 +16,9 @@ $(window).resize(function () {
     $(".day").css("height", $(window).height());
 
 });
+
+function goto(path) {
+    $("html, body").animate({
+        scrollTop: $(path).offset().top
+    }, 1500);
+};
