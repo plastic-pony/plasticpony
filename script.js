05eda8b9a60d4f32c0733a6a2efff6068480e8fd
plasticpony <izabela.bratovic@gmail.com>
2014-11-29 19:49:27 +0100

    fb login

--- script.js
+++ script.js
@@ -12,6 +12,28 @@ window.fbAsyncInit = function () {
     load_question();
 };
 
+function onLogin(response) {
+    if (response.status == 'connected') {
+        FB.api('/me?fields=first_name', function (data) {
+            var welcomeBlock = document.getElementById('fb-welcome');
+            welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+        });
+    }
+}
+
+FB.getLoginStatus(function (response) {
+    // Check login status on load, and if the user is
+    // already logged in, go directly to the welcome message.
+    if (response.status == 'connected') {
+        onLogin(response);
+    } else {
+        // Otherwise, show Login dialog first.
+        FB.login(function (response) {
+            onLogin(response);
+        }, { scope: 'email' });
+    }
+});
+
 (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) { return; }
