d0dc57d1e3ec9b0c59eb3b25d360ae1444994f61
plasticpony <izabela.bratovic@gmail.com>
2014-11-29 20:10:59 +0100

    no message

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
 ﻿var pitanja = [prvo, drugo, trece, cetvrto, peto, sesto, sedmo, osmo, deveto, deseto];
 var trenutnoPitanje = 0;
 var brojacZ = 0;
-var FB;
 
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: '747185868693965',
-        xfbml: true,
-        version: 'v2.2'
-    });
-};
-
-function onLogin(response) {
-    if (response.status == 'connected') {
-        FB.api('/me?fields=first_name', function (data) {
-            var welcomeBlock = document.getElementById('fb-welcome');
-            welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-        });
-    }
-}
-
-FB.getLoginStatus(function (response) {
-    // Check login status on load, and if the user is
-    // already logged in, go directly to the welcome message.
-    if (response.status == 'connected') {
-        onLogin(response);
-    } else {
-        // Otherwise, show Login dialog first.
-        FB.login(function (response) {
-            onLogin(response);
-        }, { scope: 'email' });
-    }
+$(document).ready(function () {
+    load_question();
 });
-
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 function next() {
     if ($("#question-holder input[type='radio']:checked").val() != undefined) {
