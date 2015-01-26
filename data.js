5c3102cad9ecb874f58c75c369b7697be440eadd
plasticpony <izabela.bratovic@gmail.com>
2015-01-26 21:42:54 +0100

    z1d

--- data.js
+++ data.js
@@ -1,99 +1,63 @@
-﻿var prvo = {
-    question: "Koje ti je najdraže jelo?",
-    a: "Sendvič sa pohanim sirom",
-    b: "Lazanje sa govedinom",
-    c: "Lazanje sa junetinom",
-    d: "Tacosi sa junetinom",
-    e: "Bilo što s mesom",
-    Z: "a"
+﻿var img001 = {
+    url: "images/13-08-31.jpg",
 }
 
-var drugo = {
-    question: "Koji ti je najdraži igrani film?",
-    a: "Wrong Turn",
-    b: "Star Wars",
-    c: "Donnie Darko",
-    d: "Fight Club",
-    e: "NE horor",
-    Z: "a"
+var img002 = {
+    url: "images/13-09-01.jpg",
 }
 
-var trece = {
-    question: "Koja ti je najdraža društvena mreža?",
-    a: "MySpace",
-    b: "Twitter",
-    c: "Facebook",
-    d: "Fer2.net",
-    e: "što su to društvene mreže?",
-    Z: "abe"
+var img003 = {
+    url: "images/13-09-02.jpg",
 }
 
-var cetvrto = {
-    question: "Koje ti je najdraže godišnje doba?",
-    a: "zima",
-    b: "proljeće",
-    c: "ljeto",
-    d: "jesen",
-    e: "sva ih jednako mrzim",
-    Z: "e"
+var img004 = {
+    url: "images/13-09-03.jpg",
 }
 
-var peto = {
-    question: "Kako najradije provodiš vikend?",
-    a: "radno",
-    b: "na lakim drogama",
-    c: "na teškim drogama",
-    d: "romantično s dečkom",
-    e: "ono što bi i Zdravka radila",
-    Z: "be"
+var img005 = {
+    url: "images/13-09-04.jpg",
 }
 
-var sesto = {
-    question: "Najdraža frizura ti je?",
-    a: "duga i kovrčava",
-    b: "ravna sa šiškama",
-    c: "undercut",
-    d: "zeleni dreadovi",
-    e: "roze boje",
-    Z: "cde"
+var img006 = {
+    url: "images/13-09-08.jpg",
 }
 
-var sedmo = {
-    question: "Najdraži gadget?",
-    a: "iPhone4s",
-    b: "MacBook Pro",
-    c: "iPhone5",
-    d: "iPhone6",
-    e: "MacBook Air",
-    Z: "abcde"
+var img007 = {
+    url: "images/13-10-01.jpg",
 }
 
-var osmo = {
-    question: "Najdraže piće ti je?",
-    a: "Čileansko bijelo vino razrijeđeno mineralnom",
-    b: "Pivo",
-    c: "Somersby",
-    d: "Gin-Tonic",
-    e: "Bilo koje bijelo vino razrijeđeno mineralnom",
-    Z: "ae"
+var img008 = {
+    url: "images/13-10-23.jpg",
 }
 
-var deveto = {
-    question: "Najdraža životinja ti je?",
-    a: "tvoj crni labrador",
-    b: "poni",
-    c: "mačka",
-    d: "kapibara",
-    e: "panda",
-    Z: "a"
+var img009 = {
+    url: "images/13-12-01.jpg",
 }
 
-var deseto = {
-    question: "Koji ti je bio najdraži predmet u školi?",
-    a: "Matematika",
-    b: "Etika",
-    c: "Informatika",
-    d: "Tjelesni",
-    e: "Markiranje",
-    Z: "be"
+var img010 = {
+    url: "images/13-12-08.jpg",
+}
+
+var img011 = {
+    url: "images/14-03-19.jpg",
+}
+
+var img012 = {
+    url: "images/14-03-30.jpg",
+}
+
+var img013 = {
+    url: "images/14-05-01.jpg",
+}
+
+var img014 = {
+    url: "images/14-05-24.jpg",
+}
+
+var img015 = {
+    url: "images/14-06-16.jpg",
+}
+
+var img016 = {
+    url: "images/14-11-10.jpg",
 }
--- script.js
+++ script.js
@@ -1,49 +1,7 @@
-﻿var pitanja = [prvo, drugo, trece, cetvrto, peto, sesto, sedmo, osmo, deveto, deseto];
-var trenutnoPitanje = 0;
-var brojacZ = 0;
+﻿var images = [img001, img002, img003, img004, img005, img006, img007, img008, img009, img010, img011, img012, img013, img014, img015, img016];
 
-$(document).ready(function () {
-    load_question();
-});
-
-function next() {
-    if ($("#question-holder input[type='radio']:checked").val() != undefined) {
-        if (pitanja[trenutnoPitanje-1].Z.indexOf($("#question-holder input[type='radio']:checked").val()) >= 0) {
-            brojacZ++;
-        }
-        $("#question-holder input[type='radio']").removeAttr("checked");
-        load_question();
-    }
-    else {
-        console.log("error");
-    }
-};
-
-function load_question() {
-    if (trenutnoPitanje < 10) {
-        $("#question").text(pitanja[trenutnoPitanje].question);
-        $(".a").text(pitanja[trenutnoPitanje].a);
-        $(".b").text(pitanja[trenutnoPitanje].b);
-        $(".c").text(pitanja[trenutnoPitanje].c);
-        $(".d").text(pitanja[trenutnoPitanje].d);
-        $(".e").text(pitanja[trenutnoPitanje].e);
-        trenutnoPitanje++;
-    }
-    else {
-        $("#radio").empty();
-        $("#header").css("background-image", "url(images/end.jpg)");
-        $(".container").css("background-color", "#ccfed9");
-        if (brojacZ >= 5) {
-            $("#question").text("Samopouzdana si i vrckava i ekstra kul mačka. U srednjoj su se svi htjeli družit s tobom! Ti ne uzimaš 'ne' kao odgovor, što znači da je više-manje sve uvijek po tvom. Većina ljudi se umori samo gledajući u tvoj raspored. Impulzivna si i ponekad te to dovede u neprilike, ali svi znaju da se samo zabavljaš.");
-        }
-        else {
-            $("#question").text("Imaš istančan smisao za humor koji gotovo nitko drugi ne razumije. Čula si za sve bendove prije nego su ih počeli puštat na radiju. Nakon dvije pive postaješ središte svakog partija, ali nitko nažalost ne dijeli s tobom to mišljenje. Uvijek pobjeđuješ u Magic: the Gathering-u.");
-        }
-    }
-};
-
-function start() {
-    $("#start-screen").hide();
-    $("#header").show();
-    $("#white-bkg").show();
+for (var i = images.length-1; i >= 0; i--) {
+    $("#poni").append("<li></li>");
+    console.log(i);
+    $("#poni li:last-child").css("background-image", "url(" + images[i].url + ")");
 };
