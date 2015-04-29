4a033988b5e298b7b55a2baee45231f42c05002e
unknown <izabela.bratovic@gmail.com>
2015-04-29 10:52:25 +0200

    responsive

--- kino/js/functions.js
+++ kino/js/functions.js
@@ -5,3 +5,19 @@
 $('#m12').hover(function () {
     $('#movie12').toggleClass("hovered");
 });
+
+$('#m21').hover(function () {
+    $('#movie21').toggleClass("hovered");
+});
+
+$('#m22').hover(function () {
+    $('#movie22').toggleClass("hovered");
+});
+
+$('#m31').hover(function () {
+    $('#movie31').toggleClass("hovered");
+});
+
+$('#m32').hover(function () {
+    $('#movie32').toggleClass("hovered");
+});
