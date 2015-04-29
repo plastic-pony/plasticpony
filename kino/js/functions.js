947394dc516bafbb17a2e7459a0a37f999a82813
unknown <izabela.bratovic@gmail.com>
2015-04-29 11:04:18 +0200

    preload images

--- kino/js/functions.js
+++ kino/js/functions.js
@@ -21,3 +21,22 @@ $('#m31').hover(function () {
 $('#m32').hover(function () {
     $('#movie32').toggleClass("hovered");
 });
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function () {
+        $('<img />').attr('src', this).appendTo('body').css('display', 'none');
+    });
+}
+
+imgArray = [
+    'images/movie11-color.jpg',
+    'images/movie12-color.jpg',
+    'images/movie21-color.jpg',
+    'images/movie22-color.jpg',
+    'images/movie31-color.jpg',
+    'images/movie32-color.jpg'
+];
+
+$(window).load(function () {
+    preload(imgArray);
+});
